e06fa53e0af83cfdd58fbd7ac702e1b32759a015
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-16 19:45:41 +0600

    palindrom learning

---
+++ palindrom.js
@@ -0,0 +1,15 @@
+// let str= "madam";
+// let revereStr= "";
+// for(let i=str.length-1;i>=0;i--){
+//     let letter=str[i];
+//     revereStr+=letter;
+// }
+// console.log(revereStr);
+// if(str === revereStr){
+//     console.log("palindrom");
+// }
+// else{
+//     console.log("Not Palindrom")
+// }
+
+
