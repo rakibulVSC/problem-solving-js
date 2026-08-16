81a59b8b6ae15ae7fe4c125b184a859e48b82724
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-16 19:00:27 +0600

    reverse string

--- reverse.js
+++ reverse.js
@@ -1,2 +1,16 @@
-let arr=[1,2,3,4];
-console.log(arr.reverse())
+// let str="programming";
+// let reverseWord="";
+// for(let i=str.length-1; i>=0; i--){
+//     let letter=str[i];
+//     reverseWord=reverseWord+letter;
+// }
+// // console.log(reverseWord);
+
+
+// let word="devoloper" ; //at first we take a variable;
+// let rebersed="";  //third we take a empty container
+// for(i=word.length-1;i>=0;i--){ //second we start for loop where i=varible.length-1;
+// let lette=word[i];  // in a variable we took i value
+// rebersed=rebersed+lette;
+// }
+// console.log(rebersed);
