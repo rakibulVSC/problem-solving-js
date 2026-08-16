60ac94c6647710cb23b2cdd18f0e47693434eacf
rakibulvsc <mrakibulhasan755@gmail.com>
2026-08-16 08:46:57 +0600

    solving problem 1

--- evenOdd.js
+++ evenOdd.js
@@ -1,8 +1,21 @@
 //check if a number is even or odd;
-const number=23;
-if(number%2 === 0){
+// const number=23;
+// if(number % 2 === 0){
+//     console.log("Even number")
+// }
+// else{
+// console.log("Odd number")
+// }
+
+
+//with functioin
+function evenOdd(number){
+    if(number % 2 === 0){
     console.log("Even number")
+    }
+    else{
+    console.log("Odd number")
+    }
 }
-else{
-console.log("Odd number")
-}
+console.log(evenOdd(25))
+
